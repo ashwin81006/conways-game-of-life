f0c41b279e7e82caa8e21f641f666a50bc3790fe
Ashy <ashwinsiva.2k25@gmail.com>
2026-05-17 20:03:36 +0530

    Game done

--- Main.java
+++ Main.java
@@ -7,23 +7,50 @@ public class Main {
     static int countneighbour(int grid[][],int r,int c)
     {
         int cc = 0;
-        if(grid[r-1][c-1]==1)
+        if(r-1 >= 0 && c-1 >=0 && grid[r-1][c-1]==1)
             cc++;
-        if(grid[r-1][c]==1)
+        if(r-1 >= 0 && grid[r-1][c]==1)
             cc++;
-        if(grid[r-1][c+1]==1)
+        if(r-1 >=0 && c+1 < cols && grid[r-1][c+1]==1)
             cc++;
-        if(grid[r][c-1]==1)
+        if(c-1 >=0 && grid[r][c-1]==1)
             cc++;
-        if(grid[r][c+1]==1)
+        if(c+1 < cols && grid[r][c+1]==1)
             cc++;
-        if(grid[r+1][c-1]==1)
+        if(r+1 < rows && c-1 >= 0 && grid[r+1][c-1]==1)
             cc++;
-        if(grid[r+1][c]==1)
+        if(r+1 < rows && grid[r+1][c]==1)
             cc++;
-        if(grid[r+1][c+1]==1)
+        if(r+1 < rows && c+1 < cols && grid[r+1][c+1]==1)
             cc++;
         return cc;
+    }
+
+    static int[][] nextgen(int grid[][])
+    {
+        int newgrid[][] = new int[rows][cols];
+        for(int i = 0;i<rows;i++)
+        {
+            for(int j = 0;j<cols;j++)
+            {
+                int ne = countneighbour(grid, i, j);
+                if(grid[i][j] == 1)
+                {
+                    if(ne == 2 || ne == 3)
+                        newgrid[i][j] = 1;
+                    else
+                        newgrid[i][j] = 0;
+                }
+                else
+                {
+                    if(ne == 3)            
+                        newgrid[i][j] = 1;
+                    else
+                        newgrid[i][j] = 0;
+                }
+            }
+        }
+        return newgrid;
     }
 
     static void print(int grid[][])
@@ -33,9 +60,9 @@ public class Main {
             for(int j = 0;j<cols;j++)
             {
                 if(grid[i][j] == 1)
-                    System.out.print("1 ");
+                    System.out.print("█ ");
                 else
-                    System.out.print("0 ");
+                    System.out.print("░ ");
             }
             System.out.println();
         }
@@ -63,9 +90,11 @@ public class Main {
         System.out.println("Enter number of iterations: ");
         int ii = in.nextInt();
 
-        for(int i = 1;i<ii;i++)
+        for(int i = 1;i<=ii;i++)
         {
             System.out.println("Iteration: " + i);
+            print(grid);
+            grid = nextgen(grid);
             Thread.sleep(2000);
         }
         in.close();
